52953c0f5712aa682a16020e92c34a828a95ecf3
Nix Tay <niiiiiiiiiiix@users.noreply.github.com>
2021-02-08 23:33:12 +0800

    Add collapsible footer

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ selectElement(".mobile-menu").addEventListener("click", () => {
 });
 
 // console.log("Hello");
+
+const coll = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
